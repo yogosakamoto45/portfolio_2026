7475d3e1dc07c06f735fed85d9150a8fb69d4b73
yogosakamoto45 <floor450@gmail.com>
2026-09-02 12:20:43 +0900

    ヒーローエリアを改修

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,28 @@
 function About() {
   return (
-    <div className="readTxtArea" id="about">
-      <div className="readTxt">
-        <div className="readTxtIn">
-          <h1 className="ttl">
-            ビジネス成長を設計する
-            <span className="catch">UI/UXデザイナー</span>
-          </h1>
-          <div className="txt">
-            <p>
-              20年以上にわたり、Web制作、UI設計、アプリ開発の現場で、エンドユーザー視点を大切にしながら業務を推進してきました。
-            </p>
-            <p>
-              とくに<b>業務支援系ツール</b>や<b>BtoBシステムのUI改善</b>を得意としています。
-            </p>
+    <section className="readTxtArea" id="about">
+        <div className="heroInner">
+
+          <div className="heroContent">
+
+            <h1 className="heroTitle">
+              <span>ビジネス成長を<br className="brPcHide"/>設計する</span>
+              <span className="catch">UI/UXデザイナー</span>
+            </h1>
+
+            <div class="heroLine"></div>
+            <div className="heroDescription">
+              <p>
+                20年以上にわたり、Web制作、UI設計、アプリ開発の現場で、<br/>
+                エンドユーザー視点を大切にしながら業務を推進してきました。
+              </p>
+              <p>
+                とくに<b>業務支援系ツール</b>や<b>BtoBシステムのUI改善</b>を得意としています。
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+    </section>
   );
 }
 
